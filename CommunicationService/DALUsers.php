c0cc68fd6713d9d9dab1739de3aa27d894e067aa
RitaNogueira <ana_rita_nogueira@hotmail.com>
2013-12-19 16:24:39 +0000

    Added some methods for inserting and changing the tags of users

--- CommunicationService/DALUsers.php
+++ CommunicationService/DALUsers.php
@@ -1,27 +1,31 @@
 <?php
 
 require_once('DAL.php');
+require_once('DALTag.php');
 
 //Insert User
     function insertUser($Username,$Password,$Email){
+		$dal = new DAL();
         $sqlUser = "INSERT INTO Users (Username) VALUES('$Username')";
-		executeQuery($sqlUser);
+		$dal->executeQuery($sqlUser);
 		$userId = mysql_insert_id();
 		$sqlLogin = "INSERT INTO Logins (UserId,Password,Email) VALUES('$userId','$Password','$Email')";
-		executeQuery($sqlLogin);
+		$dal->executeQuery($sqlLogin);
     }
 	
 	//Change User Name
 	function changeUserName($UserId,$Username){
+		$dal = new DAL();
 		$sql = "UPDATE Users SET Username = '$Username' WHERE Id = '$UserId'";
-		executeQuery($sql);
+		$dal->executeQuery($sql);
 	}
 	
 	//Login a user
 	//returns its ID or -1 if error
 	function login($Email,$Password){
+		$dal = new DAL();
 		$sqlFind = "SELECT UserId FROM Logins WHERE Email = '$Email' AND Password = '$Password'";
-		$recordset = executeNonQuery($sqlFind);
+		$recordset = $dal->executeNonQuery($sqlFind);
 		$length = mysql_num_rows($recordset);
 		if($length != 0){
 			$record = mysql_fetch_array($recordset);
@@ -36,8 +40,9 @@ require_once('DAL.php');
 	//Get a UserId using its email
 	//returns its ID or -1 if error
 	function getUserIdByEmail($Email){
+		$dal = new DAL();
 		$sqlFind = "SELECT UserId FROM Logins WHERE Email = '$Email'";
-		$recordset = executeNonQuery($sqlFind);
+		$recordset = $dal->executeNonQuery($sqlFind);
 		$length = mysql_num_rows($recordset);
 		if($length != 0){
 			$record = mysql_fetch_array($recordset);
@@ -49,10 +54,49 @@ require_once('DAL.php');
 		return $userId;
 	}
 
-	
 	function deleteUser($userId)
 	{
+		$dal = new DAL();
 		$sql = "DELETE FROM Users WHERE id = ?$userId'";
-		executeQuery($sql);
+		$dal->executeQuery($sql);
 	}
+	
+	//Insert new relation user and tag with TagId
+    function addUserTag($UserId,$TagId){
+		$dal = new DAL();
+        $sql = "INSERT INTO UserTag (UserId,TagId) VALUES('$UserId','$TagId')";
+		$dal->executeQuery($sql);
+    }
+	
+	//Insert new relation user and tag, with TagName
+	function insertUserTag($UserId,$TagName,$TypeId){
+		$dal = new DAL();
+		$sqlFind = "Select Id From Tags Where Name = '$TagName'";
+		$recordset = $dal->executeNonQuery($sqlFind);
+		$length = mysql_num_rows($recordset);
+		if($length != 0){
+			$record = mysql_fetch_array($recordset);
+			$tagId = $record["Id"];
+		}
+		else{
+			insertTag($TagName,$TypeId);
+			$tagId = mysql_insert_id();
+		}
+		addUserTag($UserId,$tagId);
+	}
+	
+	//Change User Password
+	function changeUserPassword($UserId,$Password){
+		$dal = new DAL();
+		$sql = "UPDATE Logins SET Password = '$Password' WHERE UserId = '$UserId'";
+		$dal->executeQuery($sql);
+	}
+	
+	//Change User Mood
+	function changeUserMood($UserId,$MoodId){
+		$dal = new DAL();
+		$sql = "UPDATE Users SET MoodId = '$MoodId' WHERE Id = '$UserId'";
+		$dal->executeQuery($sql);
+	}
+	
 ?>
