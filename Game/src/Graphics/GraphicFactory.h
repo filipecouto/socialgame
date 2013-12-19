84fcd5c8782a36c94890415461f58e7b764be7b7
Gil Filipe Martins de Castro <gil@Gil-Linux.site>
2013-12-19 11:15:00 +0000

    Initial work on the graphical representation classes

---
+++ Game/src/Graphics/GraphicFactory.h
@@ -0,0 +1,27 @@
+/**
+ * GraphicFactory - Header
+ *
+ * This code is part of the game module of the Social Game.
+ *
+ * CrossWeb Enterprise - 2013
+ */
+
+#ifndef GRAPHICFACTORY_H
+#define GRAPHICFACTORY_H
+
+#include "IGraphic.h"
+#include "../Models/IPerson.h"
+#include "../Models/IConnection.h"
+#include "../Models/Tag.h"
+
+/**
+ * This factory will create IGraphic's based on the object it gets
+ */
+class GraphicFactory {
+	public:
+		static IGraphic * build(IPerson * person);
+		static IGraphic * build(IConnection * person);
+		static IGraphic * build(Tag * person);
+};
+
+#endif
---
+++ Game/src/Graphics/IGraphic.h
@@ -0,0 +1,21 @@
+/**
+ * IGraphic - Header
+ *
+ * This code is part of the game module of the Social Game.
+ *
+ * CrossWeb Enterprise - 2013
+ */
+
+#ifndef IGRAPHIC_H
+#define IGRAPHIC_H
+
+/**
+ * This interface represents something visual that will represent things
+ * in the network such as people and connections.
+ */
+class IGraphic {
+	public:
+		void draw();
+};
+
+#endif
