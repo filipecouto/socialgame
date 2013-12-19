a3d6913ac9b0f9c489de66f09e2b8e93184a061a
gilfmc <gilfmcastro@gmail.com>
2013-12-19 16:58:25 +0000

    Some work on the graphical representation

---
+++ Game/src/Graphics/PersonGraphic.h
@@ -0,0 +1,30 @@
+/*
+ * PersonGraphic - Header
+ *
+ * This code is part of the game module of the Social Game.
+ *
+ * CrossWeb Enterprise - 2013
+ */
+
+#ifndef PERSONGRAPHIC_H
+#define PERSONGRAPHIC_H
+
+#include "IGraphic.h"
+#include "../Models/IPerson.h"
+
+class PersonGraphic : public IGraphic {
+	public:
+		PersonGraphic(IPerson * person);
+		
+		void load();
+		virtual void draw();
+
+		~PersonGraphic();
+
+	private:
+		IPerson * _person;
+
+		std::vector<IGraphic *> children;
+};
+
+#endif // PERSONGRAPHIC_H
