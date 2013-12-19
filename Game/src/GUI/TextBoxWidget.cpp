fd18d386155e588119db09e06cb71b58199ab96b
noobshark <filipe@linux-rciw.site>
2013-12-19 16:22:05 +0000

    Added the cursor to the textbox.

--- Game/src/GUI/TextBoxWidget.cpp
+++ Game/src/GUI/TextBoxWidget.cpp
@@ -1,139 +1,138 @@
 #include "TextBoxWidget.h"
 
-TextBoxWidget::TextBoxWidget()
-{
+TextBoxWidget::TextBoxWidget(): TextBoxWidget("", 0, 0) {
 
 }
 
-TextBoxWidget::TextBoxWidget(std::string text, GLfloat xPos, GLfloat yPos): TextWidget(text, xPos, yPos)
-{
+TextBoxWidget::TextBoxWidget(std::string text, GLfloat xPos, GLfloat yPos): TextBoxWidget(text, 1, 1, 1, 1, GLUT_BITMAP_HELVETICA_12, xPos, yPos) {
 
 }
 
-TextBoxWidget::TextBoxWidget(std::string text, GLfloat r, GLfloat g, GLfloat b, GLfloat a, void* font, GLfloat xPos, GLfloat yPos): TextWidget(text, xPos, yPos)
-{
+
+
+TextBoxWidget::TextBoxWidget(std::string text, void * font, GLfloat xPos, GLfloat yPos): TextBoxWidget(text, 1, 1, 1, 1, font, xPos, yPos) {
 
 }
 
-TextBoxWidget::TextBoxWidget(std::string text, void* font, GLfloat xPos, GLfloat yPos): TextWidget(text, xPos, yPos)
-{
+TextBoxWidget::TextBoxWidget(std::string text, GLfloat r, GLfloat g, GLfloat b, GLfloat a, GLfloat xPos, GLfloat yPos): TextBoxWidget(text, r, g, b, a, GLUT_BITMAP_HELVETICA_12, xPos, yPos) {
 
 }
 
-TextBoxWidget::TextBoxWidget(std::string text, GLfloat r, GLfloat g, GLfloat b, GLfloat a, GLfloat xPos, GLfloat yPos): TextWidget(text, xPos, yPos)
-{
+TextBoxWidget::TextBoxWidget(std::string text, GLfloat r, GLfloat g, GLfloat b, GLfloat a, void * font, GLfloat xPos, GLfloat yPos): TextWidget(text, r, g, b, a, font, xPos, yPos) {
+	cursorPosition = text.size();
+	textSize = glutBitmapLength(getFont(), (const unsigned char *) text.substr(0, cursorPosition).c_str());
+}
+
+TextBoxWidget::~TextBoxWidget() {
 
 }
 
-TextBoxWidget::~TextBoxWidget()
-{
-
+GLfloat TextBoxWidget::getMinimumWidth() {
+	return TextWidget::getMinimumWidth();
 }
 
-GLfloat TextBoxWidget::getMinimumWidth()
-{
-    return TextWidget::getMinimumWidth();
+GLfloat TextBoxWidget::getMinimumHeight() {
+	return TextWidget::getMinimumHeight();
 }
 
-GLfloat TextBoxWidget::getMinimumHeight()
-{
-    return TextWidget::getMinimumHeight();
-}
-
-void TextBoxWidget::draw()
-{
-    TextWidget::draw();
-    glBegin(GL_LINES);
-      glVertex2f(textSize, getMinimumHeight());
-      glVertex2f(textSize, 0);
-    glEnd();
-}
-
-void TextBoxWidget::drawBackground()
-{
-    TextWidget::drawBackground();
-    glBegin(GL_LINES);
-      glVertex2f(0, 0);
-      glVertex2f(getMinimumWidth(), 0);
-      glVertex2f(getMinimumWidth(), getMinimumHeight());
-      glVertex2f(0, getMinimumHeight());
-    glEnd();
-}
-
-void TextBoxWidget::onMouseEnter(int x, int y)
-{
-    TextWidget::onMouseEnter(x, y);
-}
-
-void TextBoxWidget::onMouseExit()
-{
-    TextWidget::onMouseExit();
-}
-
-GLboolean TextBoxWidget::onMouseButtonDown(int button, int x, int y)
-{
-    return TextWidget::onMouseButtonDown(button, x, y);
-}
-
-GLboolean TextBoxWidget::onMouseButtonUp(int button, int x, int y)
-{
-    return TextWidget::onMouseButtonUp(button, x, y);
-}
-
-GLboolean TextBoxWidget::onKeyDown(int key)
-{
-    std::string text = getText();
-    int currentPosition = cursorPosition;
-    switch(key) {
-    case GLUT_KEY_LEFT:
-	currentPosition--;
-	if(currentPosition < 0) {
-	  currentPosition = 0;
+void TextBoxWidget::draw() {
+	TextWidget::draw();
+	if(focused) {
+		glBegin(GL_QUADS);
+		glVertex2f(textSize, 0);
+		glVertex2f(textSize, getMinimumHeight());
+		glVertex2f(textSize + 1, getMinimumHeight());
+		glVertex2f(textSize + 1, 0);
+		glEnd();
 	}
-	cursorPosition = currentPosition;
-        break;
-    case GLUT_KEY_RIGHT:
-	currentPosition++;
-	if(currentPosition > text.length()) {
-	  currentPosition = text.length();
-	}
-	cursorPosition = currentPosition;
-        break;
-    case 8:
-	if(currentPosition > 0) {
-	  text.erase(currentPosition-1, 1);
-	  currentPosition--;
-	}
-	cursorPosition = currentPosition;
-        break;
-    case 127:
-	if(currentPosition < text.length()) {
-	  text.erase(currentPosition, 1);
-	}
-	cursorPosition = currentPosition;
-        break;
-    default:
-	text.insert(cursorPosition, 1, (char)key);
-	cursorPosition++;
-        break;
-    }
-    textSize = glutBitmapLength(getFont(), (const unsigned char*) text.substr(0,cursorPosition).c_str());
-    setText ( text );
-    return true;
 }
 
-GLboolean TextBoxWidget::onKeyUp(int key)
-{
-    return true;
+void TextBoxWidget::drawBackground() {
+	TextWidget::drawBackground();
+	glBegin(GL_LINES);
+		glVertex2f(0, 0);
+		glVertex2f(getMinimumWidth(), 0);
+		glVertex2f(getMinimumWidth(), getMinimumHeight());
+		glVertex2f(0, getMinimumHeight());
+	glEnd();
 }
 
-void TextBoxWidget::onFocus()
-{
-    Widget::onFocus();
+void TextBoxWidget::onMouseEnter(int x, int y) {
+	TextWidget::onMouseEnter(x, y);
 }
 
-void TextBoxWidget::onBlur()
-{
-    Widget::onBlur();
+void TextBoxWidget::onMouseExit() {
+	TextWidget::onMouseExit();
+}
+
+GLboolean TextBoxWidget::onMouseButtonDown(int button, int x, int y) {
+	return TextWidget::onMouseButtonDown(button, x, y);
+}
+
+GLboolean TextBoxWidget::onMouseButtonUp(int button, int x, int y) {
+	return TextWidget::onMouseButtonUp(button, x, y);
+}
+
+GLboolean TextBoxWidget::onKeyDown(int key) {
+	std::string text = getText();
+	int currentPosition = cursorPosition;
+
+	switch(key) {
+		case GLUT_KEY_LEFT:
+			currentPosition--;
+
+			if(currentPosition < 0) {
+				currentPosition = 0;
+			}
+
+			cursorPosition = currentPosition;
+			break;
+		case GLUT_KEY_RIGHT:
+			currentPosition++;
+
+			if(currentPosition > text.length()) {
+				currentPosition = text.length();
+			}
+
+			cursorPosition = currentPosition;
+			break;
+		case 8:
+
+			if(currentPosition > 0) {
+				text.erase(currentPosition - 1, 1);
+				currentPosition--;
+			}
+
+			cursorPosition = currentPosition;
+			break;
+		case 127:
+
+			if(currentPosition < text.length()) {
+				text.erase(currentPosition, 1);
+			}
+
+			cursorPosition = currentPosition;
+			break;
+		default:
+			text.insert(cursorPosition, 1, (char)key);
+			cursorPosition++;
+			break;
+	}
+
+	textSize = glutBitmapLength(getFont(), (const unsigned char *) text.substr(0, cursorPosition).c_str());
+	setText(text);
+	return true;
+}
+
+GLboolean TextBoxWidget::onKeyUp(int key) {
+	return true;
+}
+
+void TextBoxWidget::onFocus() {
+	Widget::onFocus();
+}
+
+void TextBoxWidget::onBlur() {
+	Widget::onBlur();
 }
 
