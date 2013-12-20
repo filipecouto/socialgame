a995a8855110eaeb4096752158c9274b738f9c65
Filipe Silva <pipokidpt@gmail.com>
2013-12-20 18:28:16 +0000

    Some fixes and changes to be made. The if resource needs to be moved to the DAL.php.

--- CommunicationService/InterfaceToBLL.php
+++ CommunicationService/InterfaceToBLL.php
@@ -1,18 +1,26 @@
 <?php
-	if (isset($_GET["Theme"]) and isset($_GET["Function"])){
+	if (isset($_GET["Theme"]) && isset($_GET["Function"])){
 		$theme = $_GET["Theme"];
 		$function = $_GET["Function"];
 
 		$theme = strtolower($theme);
 		$theme = ucfirst($theme);
 
-		$bllFile = "DAL" + $theme + ".php";
+		$bllFile = "BLL/".$theme.".php";
 		require_once($bllFile);
 
 		if(function_exists($function)){
-			$result = $_GET[$function]();
-			$array = get_object_vars($result);
-			var_dump(json_encode($array));
+			$result = $function();
+			if(is_resource($result)){
+				$array = array();
+				while ($row = mysql_fetch_array($result)) {
+				   $array[] = get_object_vars((object)$row);
+				}
+			}
+			else{
+				$array = get_object_vars((object)$result);
+			}
+			return(json_encode($array));
 		}
 	}
 ?>
