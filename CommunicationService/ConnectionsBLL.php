7687818b984dc1a7a0d2290e1e07d8d1d98385e2
RitaNogueira <ana_rita_nogueira@hotmail.com>
2013-12-20 16:36:12 +0000

    Creation of BLLs MinigamesBLL.php, ConnectionsBLL.php, MoodsBLL.php, NotificationsBLL.php and TagBLL.php  to use the methods from DALs

---
+++ CommunicationService/ConnectionsBLL.php
@@ -0,0 +1,27 @@
+<?php
+	require_once('DALConnection.php');
+	
+	function createConnection($UserId1, $UserdId2){
+		addConnection($UserId1, $UserdId2);
+	}
+	
+	function createConnectionMinigame($ConnectionId,$MinigameId,$Difficulty,$Score){
+		addConnectionMinigame($ConnectionId,$MinigameId,$Difficulty,$Score);
+	}
+	
+	function modifyConnectionMinigame($ConnectionId,$MinigameId,$Difficulty,$Score){
+		updateConnectionMinigame($ConnectionId,$MinigameId,$Difficulty,$Score)
+	}	
+	
+	function modifyConnectionStrength($Id,$Strength){
+		updateConnectionStrength($Id,$Strength);
+	}
+
+	function modifyConnectionState($Id,$State){
+		changeConnectionState($Id,$State);
+	}
+	
+	function getConnectionIdByUsers($User1,$User2){
+		getConnectionIdByUsers($User1,$User2);
+	}
+?>
---
+++ CommunicationService/MoodsBLL.php
@@ -0,0 +1,7 @@
+<?php
+
+	require_once('DALMood.php');
+	function createMood($description){
+		insertMood($description);
+	}
+?>
