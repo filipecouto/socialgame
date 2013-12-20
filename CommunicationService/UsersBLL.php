c4c0e3c504747a3f3c2c3d16f8a1fc8ca298251e
RitaNogueira <ana_rita_nogueira@hotmail.com>
2013-12-20 16:23:53 +0000

    Creation of UsersBLL.php to use the methods related to user from DALUsers.php and DALSession.php

---
+++ CommunicationService/UsersBLL.php
@@ -0,0 +1,80 @@
+<?php
+	require_once('DALUsers.php');
+	require_once('DALSession.php');
+	
+	function createUser($userName, $password, $email){
+		$encriptedPassword = sha256($password);
+		insertUser($userName, $encriptedPassword, $email);
+		$userId =getUserIdByEmail($email);
+		if($userId !=-1)
+		{
+			$token = token();
+			insertSession($userId,$token);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	function modifyUserName($token,$Username){
+		$userId = getUserBySession($token);
+		if($userId !=-1)
+		{
+			changeUserName($UserId,$Username);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function modifyUserPassword($token,$password){
+		$userId = getUserBySession($token);
+		if($userId !=-1)
+		{
+			$encriptedPassword = sha256($password);
+			changeUserPassword($UserId,$encriptedPassword);
+			return true;
+		}else{
+			return false;
+	}
+	
+	//Token = -1 -> token doesn't exist -> create session
+	//Token =-2 -> user doesn't exist
+	function login($Email,$Password){
+		$token = login($Email,$Password);
+		if($token == -1){
+			$userId = getUserIdByEmail($Email);
+			$newToken = token();
+			insertSession($userId, $newToken);
+			$session = getSession($newToken);
+		}else{
+			if($token == -2){
+				$session = false;
+			}else{
+				$session = getSession($newToken);
+			}
+		}
+	}
+		
+	function findUserIdByEmail($email){
+		$user = getUserIdByEmail($email);
+		if($user!=-1)
+		{
+				return $user;
+		}else{
+			return false;
+		}
+	}
+	
+	function removeUser($userId){
+		deleteUser($userId);
+	}
+	
+	function createUserTag($UserId,$TagId){
+		insertUserTag($UserId,$TagName,$TypeId);
+	}
+	
+	function modifyUserMood($UserId,$MoodId){
+		changeUserMood($UserId,$MoodId);
+	}
+?>
