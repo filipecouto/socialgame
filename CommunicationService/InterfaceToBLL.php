ea6a935ffe0725ad0124649638776f3180ae6b9a
Filipe Silva <pipokidpt@gmail.com>
2013-12-20 15:30:47 +0000

    Changed POST to GET.

--- CommunicationService/InterfaceToBLL.php
+++ CommunicationService/InterfaceToBLL.php
@@ -1,12 +1,12 @@
 <?php
-	if (isset($_POST["Theme"])	and isset($_POST["Function"])){
-		$theme = $_POST["Theme"];
-		$function = $_POST["Function"];
+	if (isset($_GET["Theme"]) and isset($_GET["Function"])){
+		$theme = $_GET["Theme"];
+		$function = $_GET["Function"];
 
 		$theme = strtolower($theme);
 		$theme = ucfirst($theme);
 
-		$bllFile = "BLL" + $theme + ".php";
+		$bllFile = "DAL" + $theme + ".php";
 		require_once($bllFile);
 
 		if(function_exists($function)){
