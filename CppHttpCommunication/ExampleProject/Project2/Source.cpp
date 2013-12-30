e149cc39a760c840f39675f59ad33784c3c5f0c1
Filipe Silva <pipokidpt@gmail.com>
2013-12-30 16:10:36 +0000

    Added Curl_Helper which is going to help parsing the Curl result into a string. Added the library Rapidjson to parse the json results from Curl. Adapted the ExampleProject with the new features. Adapted the INSTRUCTIONS.txt with the new features.

--- CppHttpCommunication/ExampleProject/Project2/Source.cpp
+++ CppHttpCommunication/ExampleProject/Project2/Source.cpp
@@ -1,20 +1,43 @@
 #include <stdio.h>
+#include <string>
 #include <iostream>
-#include <curl.h> 
+#include <document.h> //Rapidjson
+#include <CurlHelper.h>
 using namespace std;
 
+string interfaceLocation = "http://uvm001.dei.isep.ipp.pt/SocialGameCommunicationService/InterfaceToBLL";
+string params1 = "?Theme=moods&Function=getParams&Params=ola^mundo";
+string params2 = "?Theme=moods&Function=getAllMoods";
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    CURL *curl;
-    CURLcode res;
+	//Get the result
+	string result = curl_httpget(interfaceLocation+params2);
 
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://uvm001.dei.isep.ipp.pt/SocialGameCommunicationService/InterfaceToBLL?Theme=moods&Function=getAllMoods");
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
+	//Print the result
+    cout << result << endl;
+
+	//Creating a json result - example
+	//const char json[] = "{ \"hello\" : \"world\" }";
+
+	//Parse the result
+    rapidjson::Document d;
+    d.Parse<0>(result.c_str());
+
+	//Example with single return
+	//Test with params1
+    //printf("%s\n", d["data"].GetString());
+
+	//Example with array return
+	//Test with params2
+	const rapidjson::Value& a = d["data"]; // Using a reference for consecutive access is handy and faster.
+	assert(a.IsArray());
+	for (rapidjson::SizeType i = 0; i < a.Size(); i++){ // rapidjson uses SizeType instead of size_t.
+		printf("a[%d] = ", i);
+		printf("%s = ",a[i]["id"]);
+		printf("%s\n",a[i]["description"]);
+		cout << a[i]["id"].GetString() << " " << a[i]["description"].GetString() << endl;
+	}
+
 	system("pause");
-    return 0;
 }
