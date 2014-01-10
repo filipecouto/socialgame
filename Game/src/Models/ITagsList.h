0ac14063e89a3bfa92e0d143437ac45a882bf859
gilfmc <gilfmcastro@gmail.com>
2014-01-10 09:24:18 +0000

    Creation of ITagsList, the IList for Tags

---
+++ Game/src/Models/ITagsList.h
@@ -0,0 +1,20 @@
+/**
+ * ITagsList - Header
+ *
+ * This code is part of the game module of the Social Game.
+ *
+ * CrossWeb Enterprise - 2013
+ */
+
+#ifndef ITAGSLIST_H
+#define ITAGSLIST_H
+
+#include "IList.h"
+#include "IPerson.h"
+
+class ITagsList : public IList<Tag&> {
+	public:
+		virtual ~ITagsList() { }
+};
+
+#endif
