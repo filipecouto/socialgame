2ed55fa1fd328321d7e16ee638105a383690bf73
RitaNogueira <ana_rita_nogueira@hotmail.com>
2014-01-10 17:02:01 +0000

    Corrections on inserting words and categories on the Hangman database

--- CommunicationService/DAL/Hangman.php
+++ CommunicationService/DAL/Hangman.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('DAL/DAL.php');
 	
+class DAL{
 	private $servername="localhost";
     private $dbname="HangmanMinigame";
     
@@ -37,31 +37,35 @@
             die('Could not select BD: ' . mysql_error());
         }
 		$recordset = mysql_query($SQL,$conn);
-        if(!mysql_close($conn)) {
-            die('Could not close BD:' . mysql_error());
-        }
+    //    if(!mysql_close($conn)) {
+      //      die('Could not close BD:' . mysql_error());
+        //}
 		return $recordset;
 	}
+}
 	function insertWord($word, $category){
 		$id = findCategory($category);
 		if($id == false)
 		{
 			insertCategory($category);
-			$id = findCategory($category);
+			$id= findCategory($category);
 		}
-		
+		print_r($id);
 		$dal = new DAL();
-		$sql = "INSERT INTO Hangman (word, category) VALUES('$word', $category)";
-		$dal->executeQuery($sql);
+		$sql = "INSERT INTO Words (word, category) VALUES('$word', $id)";
+		$dal->executeNonQuery($sql);
 	}
 	
 	function findCategory($category)
 	{
-		%sqlFind = "SELECT id FROM Category WHERE name = $category";
-		$recordset = $dal->executeNonQuery($sqlFind);
+		$dal = new DAL();
+		$sqlFind = sprintf("SELECT id FROM Categories WHERE name = '%s'", mysql_real_escape_string($category));
+		print_r($sqlFind);
+		$recordset = $dal->executeQuery($sqlFind);
+		$array = mysql_fetch_array($recordset);
 		$length = mysql_num_rows($recordset);
 		if($length != 0){
-			$id = $recordset;
+			$id = $array["id"];
 		}
 		else{
 			$id = false;
@@ -71,8 +75,8 @@
 	
 	function insertCategory($category){
 		$dal = new DAL();
-		$sql = "INSERT INTO Category (name VALUES('$category')";
-		executeNonQuery($sql);
+		$sql = "INSERT INTO Categories (name) VALUES('$category')";
+		$dal->executeNonQuery($sql);
 	}
 	
 	function getWord()
@@ -82,15 +86,17 @@
 	}
 	function getRandomWord()
 	{
+		$dal = new DAL();
 		$result = null;
-		%sqlFind = "SELECT COUNT(1) FROM Hangman";
-		$recordset = executeQuery($sqlFind);
+		$sqlFind = "SELECT COUNT(1) FROM Words";
+		$recordset = $dal->executeQuery($sqlFind);
 		$length = mysql_num_rows($recordset);
 		if($length != 0){
 			$wordsNumber = $recordset;
+			print_r($wordsNumber);
 			$id = rand(0,$wordsNumber);
-			$sql = "SELECT h.word, c.name FROM Hangman h, Categories c WHERE h.category=c.id LIMIT $id, $id+1";
-			$recordset = executeQuery($sql);
+			$sql = "SELECT h.word, c.name FROM Words h, Categories c WHERE h.category=c.id LIMIT $id, $id+1";
+			$recordset = $dal->executeQuery($sql);
 			$length = mysql_num_rows($recordset);
 			if($length != 0){
 				$result = array("word" => $recordset[0], "category" => $recordset[1]);
