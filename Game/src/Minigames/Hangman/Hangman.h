2fb8b33e3d250909bdf1802f0ec67d9b1f09936e
gilfmc <gilfmcastro@gmail.com>
2014-01-13 18:34:11 +0000

    Hangman minigame: some help displaying underlined text

--- Game/src/Minigames/Hangman/Hangman.h
+++ Game/src/Minigames/Hangman/Hangman.h
@@ -9,7 +9,7 @@
 #include<math.h>
 #include <GL/glut.h>
 #include <algorithm>
-#include <string.h> 
+#include <string.h>
 
 
 
@@ -20,70 +20,77 @@ using namespace std;
 
 
 class HangmanMinigame : public IMinigame {
-public:
-    HangmanMinigame(GameContext * context);
+	public:
+		HangmanMinigame(GameContext * context);
 
-    virtual std::string getName();
+		virtual std::string getName();
 
-    virtual IMinigameInstance * newGame();
+		virtual IMinigameInstance * newGame();
 
-private:
-    GameContext * _context;
+	private:
+		GameContext * _context;
 
-    class HangmanInstance : public IMinigameInstance {
-    public:
-        HangmanInstance(GameContext * context);
+		class HangmanInstance : public IMinigameInstance {
+			public:
+				HangmanInstance(GameContext * context);
 
-        virtual void start();
+				virtual void start();
 
-        virtual void draw();
-        virtual void tick(int delta, int current);
+				virtual void draw();
+				virtual void tick(int delta, int current);
 
-        virtual void onKeyDown(int key, int special);
-        virtual void onKeyUp(int key, int special);
+				virtual void onKeyDown(int key, int special);
+				virtual void onKeyUp(int key, int special);
 
-        virtual void onMouseButton(int state, int button, int x, int y);
-        virtual void onMouseMove(int x, int y);
+				virtual void onMouseButton(int state, int button, int x, int y);
+				virtual void onMouseMove(int x, int y);
 
-        virtual void finish();
+				virtual void finish();
 
-        virtual ~HangmanInstance();
-        virtual void drawCircle(double distance, double pX, double pZ);
-        virtual void drawLines (GLfloat width,GLfloat beginX, GLfloat beginY, GLfloat beginZ,GLfloat endX, GLfloat endY, GLfloat endZ);
-        virtual void drawSquare(GLfloat v1x, GLfloat v1y, GLfloat v1z,GLfloat v2x, GLfloat v2y, GLfloat v2z,GLfloat v3x, GLfloat v3y, GLfloat v3z,GLfloat v4x, GLfloat v4y, GLfloat v4z);
-        virtual void drawLetterSpaces(int length);
-        virtual void determinateLetter(string chosenLetter, string word);
-	virtual void drawHangman();
-	virtual void drawLetters();
-	void getWord();
+				virtual ~HangmanInstance();
+				virtual void drawCircle(double distance, double pX, double pZ);
+				virtual void drawLines(GLfloat width, GLfloat beginX, GLfloat beginY, GLfloat beginZ, GLfloat endX, GLfloat endY, GLfloat endZ);
+				virtual void drawSquare(GLfloat v1x, GLfloat v1y, GLfloat v1z, GLfloat v2x, GLfloat v2y, GLfloat v2z, GLfloat v3x, GLfloat v3y, GLfloat v3z, GLfloat v4x, GLfloat v4y, GLfloat v4z);
+				virtual void drawLetterSpaces(int length);
+				virtual void determinateLetter(string chosenLetter, string word);
+				virtual void drawHangman();
+				virtual void drawLetters();
+				void getWord();
 
-    private:
-        int attempts;
-	int xWrong;
-	string word;
-	string category;
-	char  letterRigthPlayed;
-	string  letterWrongPlayed;
-	int wordLength;
-	bool right;
-	bool wrong;
-	int numberOfLettersRightPlayed;
-        CentralServerHangman centralServer;
-        GameContext * _context;
-        Gui * gui;
+			private:
+				int attempts;
+				int xWrong;
+				string word;
+				string category;
+				char  letterRigthPlayed;
+				string  letterWrongPlayed;
+				int wordLength;
+				bool right;
+				bool wrong;
+				int numberOfLettersRightPlayed;
+				CentralServerHangman centralServer;
+				GameContext * _context;
+				Gui * gui;
 
-        Camera camera;
+				Camera camera;
 
-        typedef struct Thing {
-            GLfloat x, y;
-            GLfloat vx, vy;
-        } Thing;
+				typedef struct Thing {
+					GLfloat x, y;
+					GLfloat vx, vy;
+				} Thing;
 
-        bool keys[26];
+				bool keys[26];
 
-        Thing thing;
-        GLfloat mx, my;
-    };
+				Thing thing;
+				GLfloat mx, my;
+
+				class LetterWidget : public TextWidget {
+					public:
+						LetterWidget(string text, GLfloat xPos, GLfloat yPos);
+						
+						virtual void drawBackground();
+				};
+		};
 };
 
 #endif //HANGMAN_H
