a2448fde4bc802034bcb4a177d14e990d5f0e59a
noobshark <filipe_jcouto@msn.com>
2014-01-14 22:13:39 +0000

    Sound
    Volume change is now supported.

--- Game/src/Models/Sound.cpp
+++ Game/src/Models/Sound.cpp
@@ -1,20 +1,21 @@
 #include "Sound.h"
 
 Sound::Sound(){
-
 }
 
 Sound::Sound(std::string filename){
 	loadSoundFile(filename);
+	_volume = 0.5f;
 }
 
 void Sound::initAudio(){
 	printf("%s\n", filename.c_str());
-	state.buffer = alutCreateBufferFromFile(filename.c_str());
-	alGenSources(1, &state.source);
-	alSourcei(state.source, AL_BUFFER, state.buffer);
-	state.key_s = AL_FALSE;
-	alSourcePlay(state.source);
+	_state.buffer = alutCreateBufferFromFile(filename.c_str());
+	alGenSources(1, &_state.source);
+	changeVolume();
+	alSourcei(_state.source, AL_BUFFER, _state.buffer);
+	_state.key_s = AL_FALSE;
+	alSourcePlay(_state.source);	
 }
 
 bool Sound::loadSoundFile(std::string filename){
@@ -23,5 +24,14 @@ bool Sound::loadSoundFile(std::string filename){
 }
 
 Sound::~Sound(){
+	alDeleteBuffers(1, _state.buffer);
+}
 
+void Sound::setVolume(float volume) {
+	_volume = volume;
+	changeVolume();
+}
+
+void Sound::changeVolume() {
+	alSourcef(_state.source, AL_GAIN, _volume);
 }
--- Game/src/Models/Sound.h
+++ Game/src/Models/Sound.h
@@ -1,7 +1,12 @@
 #ifndef ISOUND_H
 #define ISOUND_H
 
+#ifdef _WIN32
 #include <alut.h>
+#else
+#include <AL/al.h>
+#include <AL/alut.h>
+#endif
 #include <string>
 
 class Sound {
@@ -12,7 +17,6 @@ class Sound {
 	} State;
 
 	public:
-		State state;
 		std::string filename;
 
 		Sound();
@@ -20,9 +24,14 @@ class Sound {
 
 		void initAudio();
 		bool loadSoundFile(std::string filename);
-
+		void setVolume(float volume);
+		void changeVolume();
 
 		~Sound();
+		
+	private:
+		float _volume;
+		State _state;
 };
 
 #endif
